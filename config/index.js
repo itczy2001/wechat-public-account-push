5c2b7f8c46f613a576d2c461252a005ade7545e4
itczy2001 <104958228+itczy2001@users.noreply.github.com>
2022-09-18 15:18:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oHNug6nQNnSY_Y1if_uSiyEPlzFk",
+      id: "oHNug6glr3oDOMeliiuHdAqkCasg",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "QcmoghEQIuZqCix1AFiBAWejVGURveLQH3MNtU6i0mU",
       // 所在省份
